880c0f9773dcdd50c3bbf2ac82158d1309b66add
Ihor Triapitsyn <triapitsyn90@gmail.com>
2024-05-17 18:29:11 +0300

    change db

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host: "localhost",
+    host: "dpg-cp3mptol6cac73f8r9ug-a",
     port: 5432,
-    user: "postgres",
-    password: "12345",
-    database: "events"
+    user: "user",
+    password: "k5606OdcXZ9sacpUJNq6K24HrPPL7fvz",
+    database: "events_ku82"
 })
 
 module.exports = pool;
