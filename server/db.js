137b99f4c26238eaee3601412c4fa039435c56dc
Ihor Triapitsyn <triapitsyn90@gmail.com>
2024-05-17 18:46:25 +0300

    Revert "change db"
    This reverts commit 880c0f9773dcdd50c3bbf2ac82158d1309b66add.

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    host: "dpg-cp3mptol6cac73f8r9ug-a",
+    host: "localhost",
     port: 5432,
-    user: "user",
-    password: "k5606OdcXZ9sacpUJNq6K24HrPPL7fvz",
-    database: "events_ku82"
+    user: "postgres",
+    password: "12345",
+    database: "events"
 })
 
 module.exports = pool;
